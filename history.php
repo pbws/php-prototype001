ab3841ae9f2ef11d51d26a400154752a74fcb634
watanabe shota <watanabe.s.probsc@gmail.com>
2014-08-10 22:24:08 +0900

    bootstrap ナビバー周りの作成

--- history.php
+++ history.php
@@ -1,8 +1,105 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PHP　Prototype001</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- DataTables CSS -->
+        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.2/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/plug-ins/725b2a2115b/integration/bootstrap/3/dataTables.bootstrap.css">
 
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        <style type="text/css">
+            ul.nav li{
+                border-left: 1px solid #e9e9e9;
+            }
+            ul.nav li:last-child{
+                border-left: 1px solid #e9e9e9;
+                border-right: 1px solid #e9e9e9;
+            }
+            h1.pt_title {
+                background-color: #fea;
+                margin: 0 10px;
+                padding: 5px 10px;
+                color: #777;
+                border-left: 5px solid #da5;
+            }
+            div.pt_main {
+                margin-top: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <span class="navbar-brand">PHP-Prototype001</span>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#" class="glyphicon glyphicon-list"> 履歴</a></li>
+                        <li><a href="#" class="glyphicon glyphicon-plus"> 登録</a></li>
+                        <li><a href="#" class="glyphicon glyphicon-edit"> フォーム編集</a></li>
+                    </ul>
+                    <div class="nav navbar-nav navbar-right">
+                        <span class="navbar-text">ユーザ名：PHP　太郎</span>
+                        <ul class="nav navbar-nav">
+                            <li><a href="#" class="glyphicon glyphicon-off"> ログアウト</a></li>
+                        </ul>
+                    </div>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <h1 class="pt_title">履歴</h1>
+        <div class="container pt_main">
+            <table id="pt_tbl" class="cell-border hover">
+                <thead>
+                    <tr>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>C</th>
+                        <th>D</th>
+                        <th>E</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- DataTables -->
+        <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.js"></script>
+        <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/plug-ins/725b2a2115b/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+        
+        <script type="text/javascript">
+            var data = [
+                ["2014/08/01","タイプA","PHP　太郎","着手中","2014/08/02"],
+                ["2014/08/03","タイプB","PHP　二郎","着手中","2014/08/03"],
+                ["2014/08/04","タイプA","PHP　三郎","未着手","-"]
+            ];
+            $('#pt_tbl').DataTable({
+                data:data
+            });
+        </script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="ja">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap 101 Template</title>
+        <title>PHP　Prototype001</title>
 
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -20,23 +15,62 @@ and open the template in the editor.
           <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <style type="text/css">
+            div {
+                /*border: 1px solid #000;*/
+            }
+            .tmp1 button {
+                width: 50%;
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
         <div class="navbar navbar-default" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <span class="navbar-brand">Title</span>
+                    <span class="navbar-brand">タイトル</span>
                 </div>
-                <div class="nav navbar-right">
-                    <span class="navbar-text">User Name</span>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">メニュー１</a></li>
+                        <li><a href="#">メニュー１</a></li>
+                        <li><a href="#">メニュー１</a></li>
+                    </ul>
+                    <div class="nav navbar-right">
+                        <span class="navbar-text">ユーザ名</span>
+                    </div>
                 </div>
             </div>
         </div>
-        <h1>Hello, world!</h1>
-
+        <div class="container">
+            <div class="btn-group" style="width:100%;">
+                <button class="btn btn-default active">メニュー１</button>
+                <button class="btn btn-default">メニュー１</button>
+                <button class="btn btn-default">メニュー１</button>
+            </div>
+        </div>
+        
+        <div class="container tmp1">
+            <div class="row">
+                <div class="col-sm-12">
+                    <button class="btn btn-default btn-lg center-block">メニュー１</button>
+                </div>
+                <div class="col-sm-12">
+                    <button class="btn btn-default btn-lg center-block">メニュー２</button>
+                </div>
+                <div class="col-sm-12">
+                    <button class="btn btn-default btn-lg center-block">メニュー３</button>
+                </div>
+            </div>
+        </div>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="js/bootstrap.min.js"></script>
+        
+        <script type="text/javascript">
+            
+        </script>
     </body>
 </html>
